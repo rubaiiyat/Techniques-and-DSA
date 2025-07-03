90571f8b9081d4495dba6ffc78a74ddff1f42b00
rubaiiyat <abirr1516@gmail.com>
2025-07-03 17:14:41 +0600

    Idel Generator

---
+++ 01-Brute-Force/A_Ideal_Generator.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main()
+{
+    optimize();
+
+
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        int n;
+        cin>>n;
+
+        if (n%2==0) cout<<"NO"<<endl;else cout<<"YES"<<endl;
+        
+        
+    }
+    
+
+
+    
+    return 0;
+}
