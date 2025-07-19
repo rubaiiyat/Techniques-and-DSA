f6e0a8e04030e2f2a544f799d523f0a2ca5f33c8
rubaiiyat <abirr1516@gmail.com>
2025-07-19 23:48:42 +0600

    Elephant

---
+++ 03-Greedy/A_Elephant.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    optimize();
+
+
+    int x;
+    cin>>x;
+
+    cout<<(x+4)/5<<endl;
+
+    
+    
+    return 0;
+}
