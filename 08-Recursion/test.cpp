8c70618f7a9344a9061d8870538c08cc8b23986a
rubaiiyat <abirr1516@gmail.com>
2025-07-20 17:03:45 +0600

    test

---
+++ 08-Recursion/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    optimize();
+
+
+    cout<<"test is working perfectly";
+
+
+    
+    return 0;
+}
