3e2de40996c582b861042c1c79d5fd8553230347
rubaiiyat <abirr1516@gmail.com>
2025-10-21 12:19:32 +0600

    solve issue at prime factorization

--- 12-Number-Theory/Prime_Factorization/Prime_Factorization.cpp
+++ 12-Number-Theory/Prime_Factorization/Prime_Factorization.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void primeFact(int n){
     for (int i = 2; i*i <= n; i++)
     {
-        if(n%2==0){
+        if(n%i==0){
             int cnt=0;
             while (n%i==0)
             {
